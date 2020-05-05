e5a7fd6e86aca94a6ce76a373da5fdcc458fd686
KANAME-IRIE <mrexile0926@gmail.com>
2020-05-05 18:58:05 +0900

    2ndcommit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,25 @@
 class BooksController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     def index 
+        @Books = Book.all
     end
     def show
     end
     def create
-    end
+        @book = Book.new(book_params)
+    
+        respond_to do |format|
+          if @book.save
+            format.html { redirect_to @book, notice: 'Book was successfully created.' }
+            format.json { render :show, status: :created, location: @book }
+          else
+            format.html { render :new }
+            format.json { render json: @book.errors, status: :unprocessable_entity }
+          end
+        end
+      end
     def new
+        @book = Book.new
     end
     def edit
     end
@@ -14,5 +28,14 @@ class BooksController < ApplicationController
     def destroy
     end
     
+    private
+    
+    def set_book
+      @book = Book.find(params[:id])
+    end
 
+    
+    def book_params
+      params.require(:book).permit(:name)
+    end
 end
